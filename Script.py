ea80f290c3531a40928433e0d16fb45b04765552
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2025-02-15 14:44:12 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,17 @@
+#Coded by KA18 the @legend580 💛❤️
+
+class script(object):
+    START_TEXT = """<b>🤗 Hello {}
+    ɪ ᴀᴍ ᴀ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ.</b>"""
+
+    WAIT_MSG = """"<b>Processing ...</b>"""
+
+    ABOUT_TXT = """<b>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟
+ ‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/MJ_search_bot> MJ Sᴇᴀʀᴄʜ Bᴏᴛ</a>
+ ‣ My best friend : <a href='tg://settings'>Tʜɪs Pᴇʀsᴏɴ</a> 
+ ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/MJ_supportbot'>ⁱᵗᶻ ᵐᵉ ᵗᵍ 🇮🇳</a> 
+ ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ</a> 
+ ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>Pʏᴛʜᴏɴ 3</a> 
+ ‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>Mᴏɴɢᴏ ᴅʙ</a> 
+ ‣ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href='https://heroku.com'>Heroku</a> 
+ ‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ2.7.2 [sᴛᴀʙʟᴇ]</b>"""
