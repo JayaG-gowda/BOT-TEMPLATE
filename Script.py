4f05f0f4116d9559fc906e1a5e2541a7a8c500e6
Jayanna G <119968609+JayaG-gowda@users.noreply.github.com>
2025-02-16 09:25:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,17 +1,80 @@
 #Coded by KA18 the @legend580 💛❤️
 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 class script(object):
-    START_TEXT = """<b>🤗 Hello {}
-    ɪ ᴀᴍ ᴀ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ.</b>"""
+    START_MSG = """<b>🤗 Hello {},
+ɪ ᴀᴍ ᴀ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ,
+
+ɪꜰ ʏᴏᴜ ᴅᴏɴ'ᴛ ᴋɴᴏᴡ ᴛʜᴀᴛ ʜᴏᴡ ᴛᴏ ᴜꜱᴇ ᴍᴇ ᴛʜᴇɴ ᴄʟɪᴄᴋ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ 👇</b>"""
 
     WAIT_MSG = """"<b>Processing ...</b>"""
 
     ABOUT_TXT = """<b>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟
- ‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/MJ_search_bot> MJ Sᴇᴀʀᴄʜ Bᴏᴛ</a>
+ ‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/group_30_robot>Cᴏʟʟᴇɢᴇ Fᴇꜱᴛ Bᴏᴛ</a>
+ 
  ‣ My best friend : <a href='tg://settings'>Tʜɪs Pᴇʀsᴏɴ</a> 
- ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/MJ_supportbot'>ⁱᵗᶻ ᵐᵉ ᵗᵍ 🇮🇳</a> 
+ 
+ ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/legend580'>ಕನ್ನಡಿಗ 💛❤️</a> 
+ 
  ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ</a> 
+ 
  ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>Pʏᴛʜᴏɴ 3</a> 
+ 
  ‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>Mᴏɴɢᴏ ᴅʙ</a> 
+ 
  ‣ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href='https://heroku.com'>Heroku</a> 
- ‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ2.7.2 [sᴛᴀʙʟᴇ]</b>"""
+ 
+ ‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ1.0.1 [sᴛᴀʙʟᴇ]</b>"""
+
+    HELP_TEXT = """
+    <b>𒊹︎︎︎ Help text</b>
+    
+     ➪ .
+        【i】  Send your bot first and second name (eg:- <code>Sharath N</code> )
+        【ii】
+     ➪ 
+
+     """
+    
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Help 🫂', callback_data='help'),
+        InlineKeyboardButton('🧑‍🎓 About🧑‍🎓', callback_data='about')
+        ],[
+        InlineKeyboardButton('🔒 Close', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🔙 Back', callback_data='home'),
+        InlineKeyboardButton('🧑‍🎓 About 🧑‍🎓', callback_data='about')
+        ],[
+        InlineKeyboardButton('🔒 Close', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🔙 Back', callback_data='home'),
+        InlineKeyboardButton('Help 🫂', callback_data='help')
+        ],[
+        InlineKeyboardButton('🔒 Close', callback_data='close')
+        ]]
+    )
+    BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🔒 Close', callback_data='close')
+        ]]
+    )
+    AUTH_ADD_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('👁️Confirm', callback_data='addauthuser'),
+        InlineKeyboardButton('🔒 Close', callback_data='close')
+        ]]
+    )
+    AUTH_DELETE_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('👁️Confirm', callback_data='deleteauthuser'),
+        InlineKeyboardButton('🔒 Close', callback_data='close')
+        ]]
+    )
