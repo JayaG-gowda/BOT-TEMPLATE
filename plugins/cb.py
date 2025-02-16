bf65d3e19eec48570881aef5fabc19bfffd556ce
Jayanna G <119968609+JayaG-gowda@users.noreply.github.com>
2025-02-16 09:30:12 +0530

    Update cb.py

--- plugins/cb.py
+++ plugins/cb.py
@@ -5,6 +5,7 @@ from bot import Bot
 import time
 import asyncio
 from config import *
+from Script import script
 from datetime import datetime, timedelta
 from pyrogram.types import Message, CallbackQuery
 
@@ -17,5 +18,26 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.message.reply_to_message.delete()
         except:
             pass
+
+    elif data == "home":
+        await query.message.edit_text(
+            text=script.START_MSG.format(query.from_user.mention),
+            reply_markup=script.START_BUTTONS,
+            disable_web_page_preview=True
+        )
+
+    elif data == "help":
+        await query.message.edit_text(
+            text=script.HELP_TEXT,
+            reply_markup=script.HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+
+    elif data == "about":
+        await query.message.edit_text(
+            text=script.ABOUT_TXT,
+            reply_markup=script.ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
     else:
         pass
